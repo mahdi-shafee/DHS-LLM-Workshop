fb06118f2145bf6bbe787ce968742a65e151e740
mahdi-shafee <82397556+mahdi-shafee@users.noreply.github.com>
2023-10-30 15:36:31 +0330

    Update utils.py

--- chat_assistant/training/utils.py
+++ chat_assistant/training/utils.py
@@ -2,6 +2,7 @@ import random
 import torch
 from transformers import TrainerCallback, TrainingArguments, TrainerState, TrainerControl, IdeficsForVisionText2Text, AutoProcessor
 from torch.utils.data import IterableDataset
+from PIL import Image
 from datasets import load_dataset
 from tqdm import tqdm
 import warnings
@@ -38,122 +39,96 @@ class SaveDeepSpeedPeftModelCallback(TrainerCallback):
         return control
 
 
-class ConstantLengthDataset(IterableDataset):
-    """
-    Iterable dataset that returns constant length chunks of tokens from stream of text files.
-        Args:
-            tokenizer (Tokenizer): The processor used for proccessing the data.
-            dataset (dataset.Dataset): Dataset with text files.
-            infinite (bool): If True the iterator is reset after dataset reaches end else stops.
-            seq_length (int): Length of token sequences to return.
-            num_of_sequences (int): Number of token sequences to keep in buffer.
-            chars_per_token (int): Number of characters per token used to estimate number of tokens in text buffer.
-            shuffle (bool): If true, the samples in each buffer are suffled. Default is `True`.
-            add_eos_token (bool): If true, each buffer is delimited with eos token. Default is `True`.
-    """
-
-    def __init__(
-        self,
-        tokenizer,
-        dataset,
-        infinite=False,
-        seq_length=1024,
-        num_of_sequences=1024,
-        chars_per_token=3.6,
-        content_field="content",
-        shuffle=True,
-        add_eos_token=True,
-    ):
-        self.tokenizer = tokenizer
-        self.concat_token_id = tokenizer.eos_token_id
-        self.dataset = dataset
-        self.seq_length = seq_length
-        self.infinite = infinite
-        self.current_size = 0
-        self.max_buffer_size = seq_length * chars_per_token * num_of_sequences
-        self.content_field = content_field
-        self.shuffle = shuffle
-        self.add_eos_token = add_eos_token
-
-    def __iter__(self):
-        iterator = iter(self.dataset)
-        more_examples = True
-        while more_examples:
-            buffer, buffer_len = [], 0
-            while True:
-                if buffer_len >= self.max_buffer_size:
-                    break
-                try:
-                    buffer.append(next(iterator)[self.content_field])
-                    buffer_len += len(buffer[-1])
-                except StopIteration:
-                    if self.infinite:
-                        iterator = iter(self.dataset)
-                    else:
-                        more_examples = False
-                        break
-            tokenized_inputs = self.tokenizer(buffer, truncation=False)["input_ids"]
-            all_token_ids = []
-            for tokenized_input in tokenized_inputs:
-                if self.add_eos_token:
-                    tokenized_input = tokenized_input + [self.concat_token_id]
-                all_token_ids.extend(tokenized_input)
-            examples = []
-            for i in range(0, len(all_token_ids), self.seq_length):
-                input_ids = all_token_ids[i : i + self.seq_length]
-                if len(input_ids) == self.seq_length:
-                    examples.append(input_ids)
-            if self.shuffle:
-                random.shuffle(examples)
-            for example in examples:
-                self.current_size += 1
-                yield {
-                    "input_ids": torch.LongTensor(example),
-                    "labels": torch.LongTensor(example),
-                }
 
 
-def chars_token_ratio(dataset, tokenizer, data_column, nb_examples=400):
-    """
-    Estimate the average number of characters per token in the dataset.
-    """
-    total_characters, total_tokens = 0, 0
-    for _, example in tqdm(zip(range(nb_examples), iter(dataset)), total=nb_examples):
-        total_characters += len(example[data_column])
-        total_tokens += len(tokenizer(example[data_column]).tokens())
 
-    return total_characters / total_tokens
+def create_datasets(tokenizer, args, processor):
+    def convert_to_rgb(image):
+        if image.mode == "RGB":
+            return image
+    
+        image_rgba = image.convert("RGBA")
+        background = Image.new("RGBA", image_rgba.size, (255, 255, 255))
+        alpha_composite = Image.alpha_composite(background, image_rgba)
+        alpha_composite = alpha_composite.convert("RGB")
+        return alpha_composite
 
+    def ds_transforms(example_batch):
+        root_path = args.image_dataset_name
+        image_size = processor.image_processor.image_size
+        image_mean = processor.image_processor.image_mean
+        image_std = processor.image_processor.image_std
+    
+        image_transform = transforms.Compose([
+            convert_to_rgb,
+            transforms.RandomResizedCrop((image_size, image_size), scale=(0.9, 1.0), interpolation=transforms.InterpolationMode.BICUBIC),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=image_mean, std=image_std),
+        ])
+    
+        batch_prompt = []
+    
+        for images in example_batch['choices']:
+            images = images.split()
+            prompt = []
+            for i in range(len(images) // 2):
+                base_image = root_path +  '/' + images[2 * i]
+                personalized_image = root_path +  '/' + images[2 * i + 1]
+                ascii = ord(base_image[-5])
+                #if not os.path.isfile(base_image) or not os.path.isfile(personalized_image):
+                #    with open('/datasets/personalized_data_sd/final/hell.txt', 'a') as f:
+                #        f.write('ga\n')
+                #    continue
+                if ascii % 2 == 0:
+                    prompt.append(base_image)
+                    prompt.append("User: What is user's score for this image?")
+                    prompt.append("<end_of_utterance>")
+                    prompt.append("\nAssistant: User's score for this image: -")
+    
+                    prompt.append(personalized_image)
+                    prompt.append("User: What is user's score for this image?")
+                    prompt.append("<end_of_utterance>")
+                    prompt.append("\nAssistant: User's score for this image: +")
+                else:
+                    prompt.append(personalized_image)
+                    prompt.append("User: What is user's score for this image?")
+                    prompt.append("<end_of_utterance>")
+                    prompt.append("\nAssistant: User's score for this image: +")
+    
+                    prompt.append(base_image)
+                    prompt.append("User: What is user's score for this image?")
+                    prompt.append("<end_of_utterance>")
+                    prompt.append("\nAssistant: User's score for this image: -")
+    
+            batch_prompt.append(prompt)
+    
+        inputs = processor(batch_prompt, transform=image_transform, return_tensors="pt")
+        inputs["labels"] = inputs["input_ids"]
+    
+        return inputs
+    
+    dataframe = pd.read_csv(args.dataset_name)
+    user_images = defaultdict(dict)
+    for i, row in dataframe.iterrows():
+        user_images[i] = ''
+        for j in range(51, 51+21 + 4):
+            if isinstance(row[dataframe.keys()[j]], float):
+                continue
+            user_images[i] = user_images[i] + ' ' + row[dataframe.keys()[j]]
 
-def create_datasets(tokenizer, args):
-    dataset = load_dataset(args.dataset_name, use_auth_token=True, num_proc=args.num_workers)
-    train_data = dataset["train"]
-    valid_data = dataset["test"]
-    print(f"Size of the train set: {len(train_data)}. Size of the validation set: {len(valid_data)}")
-    chars_per_token = chars_token_ratio(train_data, tokenizer, args.dataset_text_field)
-    print(f"The character to token ratio of the dataset is: {chars_per_token:.2f}")
-    train_dataset = ConstantLengthDataset(
-        tokenizer,
-        train_data,
-        infinite=True,
-        seq_length=args.max_seq_length,
-        chars_per_token=chars_per_token,
-        content_field=args.dataset_text_field,
-        shuffle=True,
-        add_eos_token=False,
-    )
-    valid_dataset = ConstantLengthDataset(
-        tokenizer,
-        valid_data,
-        infinite=False,
-        seq_length=args.max_seq_length,
-        chars_per_token=chars_per_token,
-        content_field=args.dataset_text_field,
-        shuffle=False,
-        add_eos_token=False,
-    )
+    user_choices_df = pd.DataFrame.from_dict(user_images, orient='index', columns=['choices'])
+    train_ds = Dataset.from_pandas(user_choices_df)
+    ds = train_ds.train_test_split(test_size=0.1)
+    train_ds = ds['train']
+    test_ds = ds['test']
 
-    return train_dataset, valid_dataset
+    train_ds.remove_columns('__index_level_0__')
+    test_ds.remove_columns('__index_level_0__')
+    train_ds.set_transform(ds_transforms)
+    test_ds.set_transform(ds_transforms)
+    print(f"Size of the train set: {len(train_ds)}. Size of the validation set: {len(test_ds)}")
+
+    return train_ds, test_ds
 
 
 def create_and_prepare_model(args):
